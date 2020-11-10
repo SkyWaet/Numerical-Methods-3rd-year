f1b3c876f5b283d51b1f6465a003bade3912e07f
Gleb <gslepenkov@gmail.com>
2020-11-10 14:34:02 +0300

    Task 4 prepared for checking

--- Cauchy task/advancedEuler.m
+++ Cauchy task/advancedEuler.m
@@ -1,5 +1,5 @@
 function y = advancedEuler(x,y0,step)
-   y=zeros(size(x));   
+    y=zeros(size(x));   
     y(1)= y0;
     for i=2:size(x,1)
         ymiddle = y(i-1)+step*rightPart(x(i-1),y(i-1));
